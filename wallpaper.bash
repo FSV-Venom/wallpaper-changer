8666611ab4fb8e46358fa5a6971b7a9514fe6846
John Bradford <johnbradford@Johns-MacBook-Pro.local>
2020-12-10 09:30:26 -0600

    Initial commit

---
+++ wallpaper.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+# ↓↓ CHANGE to the full path of your desired wallpaper ↓↓
+desiredWallpaper="/Users/johnbradford/Desktop/wallpaper.jpg"
+
+
+# Find the path of the active wallpaper
+activeWallpaper="$(osascript -e 'tell application "System Events" to tell first Desktop to get its picture')"
+
+
+# Output paths of both variables in the terminal
+echo $desiredWallpaper
+echo $activeWallpaper
+
+
+# Find if the active wallpaper is different than the desired wallpaper
+if [ "$activeWallpaper" != "$desiredWallpaper" ]
+then
+
+echo "Changing wallpaper..."
+
+# Change the wallpaper to the desired wallpaper
+osascript << EOF
+    tell application "Finder"
+    set desktop picture to POSIX file "$desiredWallpaper"
+    end tell
+EOF
+
+fi
